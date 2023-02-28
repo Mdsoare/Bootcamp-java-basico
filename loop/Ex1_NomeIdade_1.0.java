bfd38f34a97d240bcf6fb8f6fb8fc412941f016d
Marcelo Soares <marcelo.soares92@gmail.com>
2023-02-28 18:00:41 -0300

    Adicionando diretório loop e arquivos de exercícios resolvidos

---
+++ loop/Ex1_NomeIdade_1.0.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+* Objetivo: Fazer um programa que leia um conjunto de dois valores,
+* o primeiro representando o nome e o segundo a idade.
+* (OBS: Pare o programa inserindo o valor "0" no campo nome).
+*
+* @author Marcelo Soares
+* @version 1.0
+* @since 2023-02-28
+*/
+
+public class Ex1_NomeIdade {
+    public static void main(String args[]) {
+		//instanciando objeto tipo "Scanner" e declarando variaveis:
+        Scanner ler = new Scanner(System.in);
+		String nome;
+        int idade;
+
+		//Criando laco	
+        while(true){
+			//lendo dados
+            ler.nextLine();
+            System.out.print("Digite seu nome: ");
+            nome = ler.next();
+			// Testando valor da variavel nome, se "0" finaliza com "break"
+            if (nome.equals("0")) break;
+            //OBS: Nao ha tratamento de erro nesta versao          
+            System.out.print("Digite sua idade: ");
+            idade = ler.nextInt();
+        }
+		//Exibindo mensagem apos sair do loop(laco)
+        System.out.println("...\n");
+        System.out.println("Fim da execucao!");
+    }
+}
