3e96ffcb48923c646f4aa777b7d1d3a9705e8bfc
Marcelo Soares <marcelo.soares92@gmail.com>
2023-03-01 15:24:09 -0300

    Adição do exercício Ex6_Fatorial_1.1.java

---
+++ loop/Ex6_Fatorial_1.1.java
@@ -0,0 +1,37 @@
+/**
+* Objetivo: Desenvolver um programa que calcule 
+* o fatorial de um numero inteiro fornecido pelo usuario
+* Ex.: 5! = 120
+*
+* Uso do for no logar do while
+*
+* @author Marcelo Soares
+* @version 1.1
+* @since 2023-03-01
+*/
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int fatorial;
+		
+	    System.out.print("Digite um numero inteiro positivo: ");
+		fatorial = scanner.nextInt();
+    
+		if (fatorial < 0) {
+			System.out.println("Numero invalido. O numero deve ser inteiro e positivo.");
+		} else if (fatorial == 0 || fatorial == 1) {
+			System.out.println(fatorial + "! = 1");
+		} else {
+			int numero = fatorial;
+			for (int i = fatorial-1; i >= 1; i--){
+				fatorial *= i;
+			}
+			System.out.println(numero + "! = " + fatorial);
+		}
+		//System.out.println("Fim do programa!");
+		scanner.close();
+	}
+}
+
