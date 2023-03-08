ecccd5bffd45567207fe4fd93c3facb8d9ca2aed
Marcelo Soares <marcelo.soares92@gmail.com>
2023-03-08 11:09:53 -0300

    Criação dos atributos cor, modelo e capacidadeTanque

--- 05 - POO/Carro.java
+++ 05 - POO/Carro.java
@@ -1,11 +1,17 @@
 /**
 * Classe de exemplo para o exercício de Orientação a Objetos
+* Criação dos atributos "cor", "modelo" e "capacidadeTanque"
 * Aula: 03
+* 
 * @author Marcelo Soares
-* @version 1.0
+* @version 1.1
 * @since 2023-03-08
 */
 
 class Carro{
+	
+	String cor;
+	String modelo;
+	int capacidadeTanque;
 	
 }
