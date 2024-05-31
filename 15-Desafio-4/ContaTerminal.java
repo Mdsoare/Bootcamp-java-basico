d63f065873f098c71d990db47392e3c7f993cd33
Marcelo Soares <106860930+Mdsoare@users.noreply.github.com>
2024-05-30 23:28:29 -0300

    Feat: Adiciona solução ao desafio

---
+++ 15-Desafio-4/ContaTerminal.java
@@ -0,0 +1,39 @@
+public class ContaTerminal {
+
+    private String nomeCliente;
+    private String agencia;
+    private int numeroConta;
+    private double saldo;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ 15-Desafio-4/Main.java
@@ -0,0 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            ContaTerminal conta1 = new ContaTerminal();
+            System.out.println("Benvindo ao sistema bancário!");
+
+            System.out.println("Digite seu nome: ");
+            conta1.setNomeCliente(sc.nextLine());
+
+            System.out.println("Digite sua agencia: ");
+            conta1.setAgencia(sc.nextLine());
+
+            System.out.println("Digite o número da conta: ");
+            conta1.setNumeroConta(sc.nextInt());
+            sc.nextLine();
+
+            System.out.println("Informe seu saldo: ");
+            conta1.setSaldo(sc.nextDouble());
+            sc.nextLine();
+
+            System.out.println(String.format("Olá %s, obrigado por criar uma conta em nosso banco, " +
+                            "sua agência é %s, conta %d e seu saldo de R$ %.2f já está disponível para saque."
+                    , conta1.getNomeCliente(), conta1.getAgencia(), conta1.getNumeroConta()
+                    , conta1.getSaldo()));
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
